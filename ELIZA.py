2ae53259a446843e729c18bbe6bb64d05901e829
MrSize001 <75929039+LOIC-only-one@users.noreply.github.com>
2023-01-22 15:11:24 +0100

    V2.0

--- ELIZA.py
+++ ELIZA.py
@@ -19,40 +19,32 @@ $$$$$$$$\ $$$$$$$$\ $$$$$$\ $$$$$$$$\ $$ |  $$ |
                                                 
 (Rogerian Psychotherapist)                                              
 	''')
-user_name = input(str("What's yout name ? "))
-print("\n")
-eliza_welcome = "Hello "+user_name+" I'm ELIZA, I'm here to help yu!\nWhat's wrong ?\n"
-print(eliza_welcome)
+import re
 
-li_0 = ['sorry','Sorry', 'homesick', 'gloomy', 'sad', 'unhappy', 'sob', 'hurt', 'depressed', 'depressing', 'upset', 'pain', 'sadness', 'sarrow', 'grief']
-str0 = " ".join(li_0)
-li_1 = ['french', 'france', 'francais', 'français', 'Paris', 'paris', 'english', 'anglais']
+# Réponses prédéfinies
+reponses = {
+  'hello': 'Bonjour. Comment puis-je vous aider aujourd\'hui ?',
+  'goodbye': 'Au revoir. J\'espère que vous avez une belle journée.',
+  'thank you': 'De rien.',
+  'j\'ai besoin (.*)': 'Pourquoi avez-vous besoin de {}?',
+  'je veux (.*)': 'Que cela signifierait-il pour vous si vous obteniez {}?',
+  'je (.*)': 'Pourquoi {}?',
+  'comment (.*)':"Pourquoi voulez vous savoir cela ?",
+  
+  '.*': 'Désolé, je ne comprends pas ce que vous voulez dire.'
+}
 
-while jeu == 0:
-	file = open('user_data.txt', "w") 
-	user_answer = input(user_name+": ")
-	user_answer == str
-	file.write(user_answer)
+def reponse_eliza(message):
+    # Recherche de correspondances
+    for pattern, response in reponses.items():
+        match = re.search(pattern, message)
+        if match:
+            # remplacement de (.*) avec la chaine capturée
+            if '{}' in response:
+                phrase = match.group(1)
+                return response.format(phrase)
+            else:
+                return response
 
-	if user_answer == 'c':
-		print('\nELIZA: Goodbye '+user_name)
-		time.sleep(2)
-		quit()
-
-	if li_0[0] in user_answer:
-		user_answer1 = user_answer
-		liste_sro = ["\nELIZA: It's not cool to repeat the same thing over and over again\n",'\nELIZA: Why are you repeating '+user_answer+'\n']
-		ok = random.choice(liste_sro)
-		print(ok)
-
-	
-	#if li_0[2] or li_0[3] in user_answer:
-		#print("\nOh no, I'm going to get a coffee and you explain to me afterwards\n") or print("\nIm going to get something to drink\n")
-
-
-	#if user_answer not in str0:
-		#els = ["\nELIA: This is not the subject\n","\nELIZA: I do not understand\n"]
-		#elsee = random.choice(els)
-		#print(elsee)
-	
-	file.close()
+# Test de la fonction
+print(reponse_eliza("hello"))
