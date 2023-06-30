6e2421a33ae9c0a88725e5501058490e049f62ec
MrSize001 <75929039+LOIC-only-one@users.noreply.github.com>
2023-06-30 16:38:49 +0200

    Update ELIZA.py
    Add user input.

--- ELIZA.py
+++ ELIZA.py
@@ -1,10 +1,7 @@
-#coding:utf-8;
-# ELIZA is an IA, wich simulated Rogerian Psychotherapist
-# Rogerian argument = recherche de terrain d'entente
 from os import close
 import time
 import random
-jeu = 0
+import re
 
 print('''
 
@@ -18,33 +15,34 @@ $$$$$$$$\ $$$$$$$$\ $$$$$$\ $$$$$$$$\ $$ |  $$ |
 \________|\________|\______|\________|\__|  \__|
                                                 
 (Rogerian Psychotherapist)                                              
-	''')
-import re
+''')
 
-# Réponses prédéfinies
 reponses = {
-  'hello': 'Bonjour. Comment puis-je vous aider aujourd\'hui ?',
-  'goodbye': 'Au revoir. J\'espère que vous avez une belle journée.',
-  'thank you': 'De rien.',
-  'j\'ai besoin (.*)': 'Pourquoi avez-vous besoin de {}?',
-  'je veux (.*)': 'Que cela signifierait-il pour vous si vous obteniez {}?',
-  'je (.*)': 'Pourquoi {}?',
-  'comment (.*)':"Pourquoi voulez vous savoir cela ?",
-  
-  '.*': 'Désolé, je ne comprends pas ce que vous voulez dire.'
+    'hello': 'Bonjour. Comment puis-je vous aider aujourd\'hui ?',
+    'goodbye': 'Au revoir. J\'espère que vous avez une belle journée.',
+    'thank you': 'De rien.',
+    'j\'ai besoin (.*)': 'Pourquoi avez-vous besoin de {}?',
+    'je veux (.*)': 'Que cela signifierait-il pour vous si vous obteniez {}?',
+    'je (.*)': 'Pourquoi {}?',
+    'comment (.*)':"Pourquoi voulez vous savoir cela ?",
+    '.*': 'Désolé, je ne comprends pas ce que vous voulez dire.'
 }
 
 def reponse_eliza(message):
-    # Recherche de correspondances
     for pattern, response in reponses.items():
         match = re.search(pattern, message)
         if match:
-            # remplacement de (.*) avec la chaine capturée
             if '{}' in response:
                 phrase = match.group(1)
                 return response.format(phrase)
             else:
                 return response
 
-# Test de la fonction
-print(reponse_eliza("hello"))
+# Interaction avec l'utilisateur
+while True:
+    message = input("> ")
+    if message.lower() == "exit":
+        print(reponse_eliza("goodbye"))
+        break
+    else:
+        print(reponse_eliza(message))
